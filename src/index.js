84c3cc45bb878f2e536a4a27c2f02119ff73dd76
GhouI <flareonverm@outlook.com>
2023-05-28 20:38:24 +0100

    Update index.js
    added origin.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app =  express();
 
 const port = process.env.PORT || 3000;
-app.use(cors());    
+app.use(cors({"origin": true}));    
 app.use(express.json())
 // Create a database connection pool with connection limits
 const pool = mysql.createPool({
